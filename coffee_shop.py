e741297bafc101fe04bd473734f44a2e453b8d31
arifa <arifashaik050@gmail.com>
2025-08-29 00:59:34 +0530

    Add Day 2 Python exercise

---
+++ coffee_shop.py
@@ -0,0 +1,8 @@
+customer_name = "YourName"
+coffee_count = 3
+price_per_coffee = 4.25
+is_paid = True
+total = coffee_count * price_per_coffee
+if is_paid:
+    total = total * 0.9  # 10% discount
+print("Receipt for", customer_name, "Total: $", total)

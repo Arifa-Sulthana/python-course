60dd3b2aaa1faaff003ced7a022d484f36bbf48e
arifa <arifashaik050@gmail.com>
2025-09-04 00:09:38 +0530

    updated the palindrome code

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,13 @@
-n=120
+n=-13
 s_n=str(n)
 revered_sn=s_n[::-1]
 print(revered_sn)
 
-if int(revered_sn)==n:
-    print("The given number is palindrome")
+if n<0:
+    print("false")
+elif n==0 or len(n)==1:
+    print("true")
+elif int(revered_sn)==n:
+    print("true")
 else:
-    print("The given number is not a palindrome")
+    print("false")

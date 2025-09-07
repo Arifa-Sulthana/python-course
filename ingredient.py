a26049c96fd67230ab4097faccbf3a5e4bf8e852
arifa <arifashaik050@gmail.com>
2025-09-07 23:46:20 +0530

    add ingredient.py challenge to the list

---
+++ ingredient.py
@@ -0,0 +1,6 @@
+ingredient= input("What's the ingredient? ")
+# def roman_to_integer(ingredient):
+if (ingredient.startswith("s")):
+    print('Spicy Ingredient!')
+else:
+    print("It's not spicy ingredient")
